7b1be5a755cea23780ed49acd0eacfc67ca68845
alves-dev <alvesmoreiraigor@gmail.com>
2025-11-14 11:30:56 -0300

    feat: test session

--- src/main/java/com/opencerts/test/TestSessionDTO.java
+++ src/main/java/com/opencerts/test/TestSessionDTO.java
@@ -3,10 +3,13 @@ package com.opencerts.test;
 public record TestSessionDTO(
         String identifier,
         int totalCorrect,
-        int totalError
+        int totalError,
+        int percentageCorrect
 ) {
 
     public TestSessionDTO(TestSession test) {
-        this(test.identifier(), test.totalCorrect(), test.totalError());
+        int total = test.totalCorrect() + test.totalError();
+        int percentage = total == 0 ? 0 : (int) Math.round(test.totalCorrect() * 100.0 / total);
+        this(test.identifier(), test.totalCorrect(), test.totalError(), percentage);
     }
 }
